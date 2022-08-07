c398996abc6bbe3958655a556825f3382667f252
stepGT <stepGT@mail.ru>
2022-08-07 23:06:36 +1000

    Added Post controller

---
+++ controllers/PostController.js
@@ -0,0 +1,133 @@
+import PostModel from '../models/Post.js';
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec();
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось получить статьи!',
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postID = req.params.id;
+    PostModel.findOneAndUpdate(
+      {
+        _id: postID,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Не удалось вернуть статью',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Статья не найдена',
+          });
+        }
+
+        res.json(doc);
+      },
+    ).populate('user');
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось получить статью!',
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userID,
+    });
+    const post = await doc.save();
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось создать статью!',
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postID = req.params.id;
+
+    PostModel.findOneAndDelete(
+      {
+        _id: postID,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Не удалось удалить статью',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Статья не найдена',
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postID = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postID,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userID,
+        tags: req.body.tags,
+      },
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось обновить статью',
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation } from './validations.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import { getMe, login, register } from './controllers/UserController.js';
+import { create, getAll, getOne, remove, update } from './controllers/PostController.js';
 
 mongoose
   .connect('mongodb+srv://admin:wwwwww@cluster0.7nouj.mongodb.net/blog?retryWrites=true&w=majority')
@@ -16,11 +17,17 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post('/auth/login', login);
+app.post('/auth/login', loginValidation, login);
 
 app.post('/auth/register', registerValidation, register);
 
 app.get('/auth/me', checkAuth, getMe);
+
+app.get('/posts', getAll);
+app.get('/posts/:id', getOne);
+app.post('/posts', checkAuth, postCreateValidation, create);
+app.delete('/posts/:id', checkAuth, remove);
+app.patch('/posts/:id', checkAuth, update);
 
 app.listen(4444, (err) => {
   if (err) console.log(err);
