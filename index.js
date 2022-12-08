4e46ced57d99acce014c415671e523f1c93ce676
Andrey Zuev <stepgt@mail.ru>
2022-12-09 09:47:39 +1000

    Added handleValidationErrors middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { registerValidation, loginValidation, postCreateValidation } from './val
 import checkAuth from './utils/checkAuth.js';
 import { getMe, login, register } from './controllers/UserController.js';
 import { create, getAll, getOne, remove, update } from './controllers/PostController.js';
+import handleValidationErrors from './utils/handleValidationErrors.js';
 
 mongoose
   .connect('mongodb+srv://admin:wwwwww@cluster0.7nouj.mongodb.net/blog?retryWrites=true&w=majority')
@@ -30,9 +31,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.post('/auth/login', loginValidation, login);
+app.post('/auth/login', loginValidation, handleValidationErrors, login);
 
-app.post('/auth/register', registerValidation, register);
+app.post('/auth/register', registerValidation, handleValidationErrors, register);
 
 app.get('/auth/me', checkAuth, getMe);
 
@@ -43,9 +44,9 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 });
 app.get('/posts', getAll);
 app.get('/posts/:id', getOne);
-app.post('/posts', checkAuth, postCreateValidation, create);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, create);
 app.delete('/posts/:id', checkAuth, remove);
-app.patch('/posts/:id', checkAuth, update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, update);
 
 app.listen(4444, (err) => {
   if (err) console.log(err);
