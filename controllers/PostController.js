2175602074beedf1f4d064fe147b290044fb4ac5
Andrey Zuev <stepgt@mail.ru>
2023-02-02 09:54:26 +1000

    Fixed update controller. Field tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -117,7 +117,7 @@ export const update = async (req, res) => {
         text: req.body.text,
         imageUrl: req.body.imageUrl,
         user: req.userID,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
       },
     );
 
